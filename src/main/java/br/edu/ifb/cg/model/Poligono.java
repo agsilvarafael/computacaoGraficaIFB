12286458e239416735513811749a82ecaff8b5ec
Jerônimo Hermano <jeronimohermano.nc@gmail.com>
2018-10-11 20:08:10 -0300

    Mudança da classe Envelope de interna a polígono para uma classe independente.

--- src/main/java/br/edu/ifb/cg/model/Poligono.java
+++ src/main/java/br/edu/ifb/cg/model/Poligono.java
@@ -5,11 +5,12 @@
  */
 package br.edu.ifb.cg.model;
 
-import java.util.List;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
- *
- * @author jeronimo
+ * @author Jeronimo Hermano
  */
 public class Poligono {
     
@@ -17,33 +18,45 @@ public class Poligono {
     private Integer lados;
     private Integer indiceArestaInicial;
     private Integer indiceArestaFinal;
-    private List<Aresta> arestas;
+    private Color corPreenchimento;
+    private Envelope envelope;
 
-    public Poligono(Integer indice, List<Aresta> arestas) {
-        if(arestas.isEmpty() || !Poligono.isPoligono(arestas))
-            throw new IllegalArgumentException("origem");
-        this.indice = indice;
-        this.lados = arestas.size();
-        this.arestas = arestas;
-        this.indiceArestaInicial = arestas.get(0).getIndice();
-        this.indiceArestaFinal = arestas.get(lados - 1).getIndice();
-    }    
+    /**
+     * Construtor padrão da classe deixado para caso seja necessário instanciar
+     * um objeto sem atribuição direta
+     */
+    public Poligono() {}
 
+    /**
+     * Construtor que recebe os índices das arestas inicial e final que o compõe.
+     * @param indiceArestaInicial Integer: indice da aresta inicial em MatrizArestas.
+     * @param indiceArestaFinal Integer: indice da aresta final em MatrizArestas.
+     * @see MatrizArestas
+     */
     public Poligono(Integer indiceArestaInicial, Integer indiceArestaFinal) {
         this.lados = indiceArestaFinal - indiceArestaInicial + 1;
         this.indiceArestaInicial = indiceArestaInicial;
         this.indiceArestaFinal = indiceArestaFinal;
+        this.envelope = new Envelope(this);
+        this.envelope.calculaEnvelopamento();
     }
-    
-    public static boolean isPoligono(List<Aresta> arestas){
-        return arestas.get(0).getOrigem().
-                equals(arestas.get(arestas.size() - 1).getDestino());
+    /**
+     * Construtor que recebe os índices das arestas inicial e final que o compõe
+     * e o próprio índice dentro da MatrizPoligonos.
+     * @param indiceArestaInicial Integer: indice da aresta inicial em MatrizArestas.
+     * @param indiceArestaFinal Integer: indice da aresta final em MatrizArestas.
+     * @see MatrizArestas
+     * @see MatrizPoligonos
+     */
+    public Poligono(Integer indiceArestaInicial, Integer indiceArestaFinal, Integer indice) {
+        this.indice = indice;
+        this.indiceArestaInicial = indiceArestaInicial;
+        this.indiceArestaFinal = indiceArestaFinal;
+        this.lados = indiceArestaFinal - indiceArestaInicial + 1;
+        this.envelope = new Envelope(this);
+        this.envelope.calculaEnvelopamento();
     }
-    
-    public boolean isClose(Vertice2D vertice){
-        return arestas.get(0).getOrigem().equals(vertice);
-    }
-    
+       
     public Integer getIndice() {
         return indice;
     }
@@ -74,14 +87,15 @@ public class Poligono {
 
     public void setIndiceArestaFinal(Integer indiceArestaFinal) {
         this.indiceArestaFinal = indiceArestaFinal;
+        this.envelope.calculaEnvelopamento();
     }
 
-    public List<Aresta> getArestas() {
-        return arestas;
+    public Color getCorPreenchimento() {
+        return corPreenchimento;
     }
 
-    public void setArestas(List<Aresta> arestas) {
-        this.arestas = arestas;
+    public void setCorPreenchimento(Color corPreenchimento) {
+        this.corPreenchimento = corPreenchimento;
     }
     
 }
