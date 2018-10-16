965fc7d75b03f9d407eb548d5763f9e3dda0047b
Jerônimo Hermano <jeronimohermano.nc@gmail.com>
2018-10-16 14:37:39 -0300

    Funções de rotação e espelhamento.

--- src/main/java/br/edu/ifb/cg/controller/TransofrmacoesSobreVertices.java
+++ src/main/java/br/edu/ifb/cg/controller/TransofrmacoesSobreVertices.java
@@ -17,9 +17,9 @@ public class TransofrmacoesSobreVertices {
     
     /**
      * 
-     * @param indiceP
-     * @param dX
-     * @param dY 
+     * @param indiceP Índice do poligono que receberá a transformação.
+     * @param dX Descolamento em pixels no eixo X.
+     * @param dY Deslocamento em pixels no eixo Y.
      */
     public void translacao(Integer indiceP, Integer dX, Integer dY){
         for (Vertice2D vertice : MatrizPoligonos.getInstance().getVerticesPoligono(indiceP)) {
@@ -47,22 +47,24 @@ public class TransofrmacoesSobreVertices {
         Integer novoXMaior = 0, novoYMaior = 0;
         
         Envelope envelope = MatrizPoligonos.getInstance().getPoligono(indiceP).getEnvelope();
+        Integer ajusteX = envelope.getMenor().getX() * sX - envelope.getMenor().getX();
+        Integer ajusteY = envelope.getMenor().getY() * sY - envelope.getMenor().getY();
         for(Vertice2D v2 : MatrizPoligonos.getInstance().getVerticesPoligono(indiceP)){
             //Modifica apenas se o ponto não for parte da lateral esqueda do envelope
             if(!v2.getX().equals(envelope.getMenor().getX())){
-               v2.setX(v2.getX() * sX - envelope.getMenor().getX());
-               //Modifica a borda direita do envelope
-               if(v2.getX().compareTo(novoXMaior) > 0){
-                   novoXMaior = v2.getX();
-               }
+                v2.setX(v2.getX() * sX - ajusteX);//Posição nova com ajuste
+                //Modifica a borda direita do envelope
+                if(v2.getX().compareTo(novoXMaior) > 0){
+                    novoXMaior = v2.getX();
+                }
             }
             //Modifica apenas se o ponto não for parte da lateral inferior do envelope
             if(!v2.getY().equals(envelope.getMenor().getY())){
-               v2.setY(v2.getY() * sY - envelope.getMenor().getY()); 
-                //Modifica a borda superior do envelope
-               if(v2.getY().compareTo(novoYMaior) > 0){
-                   novoYMaior = v2.getY();
-               }
+                v2.setY(v2.getY() * sY - ajusteY); //Posição nova com ajuste
+                 //Modifica a borda superior do envelope
+                if(v2.getY().compareTo(novoYMaior) > 0){
+                    novoYMaior = v2.getY();
+                }
             }
         }
         //Modifica a borda direita do envelope
@@ -76,4 +78,32 @@ public class TransofrmacoesSobreVertices {
         
     }
     
+    /**
+     * 
+     * @param indiceP
+     * @param anguloGraus 
+     */
+    public void rotacao(Integer indiceP, Double anguloGraus){
+        Double xAux, yAux;
+        for (Vertice2D vertice : MatrizPoligonos.getInstance().getVerticesPoligono(indiceP)) {
+            xAux = Math.cos(Math.toRadians(anguloGraus)) * vertice.getX().doubleValue() -
+                   Math.sin(Math.toRadians(anguloGraus)) * vertice.getY().doubleValue();
+            yAux = Math.sin(Math.toRadians(anguloGraus)) * vertice.getX().doubleValue() +
+                   Math.cos(Math.toRadians(anguloGraus)) * vertice.getY().doubleValue();
+            vertice.setX(xAux.intValue());
+            vertice.setY(yAux.intValue());
+        }
+    }
+    
+    public void espelhamento(Integer indiceP, Boolean inverteX, Boolean inverteY){
+        Envelope e = MatrizPoligonos.getInstance().getPoligono(indiceP).getEnvelope();
+        for(Vertice2D vertice : MatrizPoligonos.getInstance().getVerticesPoligono(indiceP)){
+            if(inverteX){
+                vertice.setX(-vertice.getX() + e.getMenor().getX() + e.getMaior().getX());
+            }
+            if(inverteY){
+                vertice.setY(-vertice.getY() + e.getMenor().getY() + e.getMaior().getY());
+            }
+        }
+    }
 }
